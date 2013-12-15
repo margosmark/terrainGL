560d679eaa713c033ebaf4c81049d645b25f4e75
margosmark <mark.arslan@gmail.com>
2013-12-14 22:36:59 -0500

    Can make a window, following http://duriansoftware.com/joe/An-intro-to-modern-OpenGL.-Chapter-2:-Hello-World:-The-Slideshow.html

--- terrainGL/terrainGL/terrainGL.cpp
+++ terrainGL/terrainGL/terrainGL.cpp
@@ -1,11 +1,45 @@
 // terrainGL.cpp : main project file.
 
 #include "stdafx.h"
+#include "glew.h"
+#include "glut.h"
 
 using namespace System;
 
-int main(array<System::String ^> ^args)
+static int make_resources(void)
 {
-    Console::WriteLine(L"Hello World");
-    return 0;
+	return 1;
+}
+
+static void update_fade_factor(void)
+{
+}
+
+static void render(void)
+{
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutInitWindowSize(400, 300);
+	glutCreateWindow("Hello World");
+	glutDisplayFunc(&render);
+	glutIdleFunc(&update_fade_factor);
+	glewInit();
+	if (!GLEW_VERSION_2_0) {
+		//fprintf(stderr, "OpenGL 2.0 not available\n");
+		return 1;
+	}
+	if (!make_resources()) {
+		//fprintf(stderr, "Failed to load resources\n");
+		return 1;
+	}
+
+	glutMainLoop();
+	return 0;
 }
